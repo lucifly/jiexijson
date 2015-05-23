a9332f5c57e510665aa0b806ee57f351ea7500f1
lucifly <xilinjuebi@163.com>
2015-05-24 00:26:28 +0800

    基本架构完成，下一步完成组件的补充

--- http_get.js
+++ http_get.js
@@ -32,7 +32,7 @@
 
 var http = require('http');
 var urllib = require('url');
-
+var test = "" ;
 var port = 3000;
 //var data = {'name': 'jifeng', 'company': 'taobao'};
 
@@ -44,18 +44,85 @@ http.createServer(function(req, res){
   var to_where = "" ;
   to_where = params.query;
   
-  console.log("real_ulr: "+to_where.reqtypr);
+  console.log("real_type: "+to_where.reqtypr);
   var req_type = to_where.reqtypr;
-
+var data;
   //判断请求类型,13使用包装后第三方请求,需转手,11使用本地服务,不需转手
   if(req_type == 13)
   {
       console.log("real_ulr: "+to_where.real_ulr);//real_ulr中存放真实请求地址
-      console.log("coor    : "+to_where.coor);//其他请求相关数据
-      var data = {'r_ulr' : (to_where.real_ulr +"&coor="+to_where.coor)};
-      /**此处还需加入转手相关代码 */
+      var ture_ulr = to_where.real_ulr;
+      if( ture_ulr == "http://api.map.baidu.com/location/ip?ak=wROHF9YIK6wRPbf4fY2tmkur" )
+      {//百度地图iptoadd 
+          //http://api.map.baidu.com/location/ip?ak=wROHF9YIK6wRPbf4fY2tmkur
+          console.log("coor    : "+to_where.coor);//其他请求相关数据
+          var t_coor = to_where.coor;
+//          var data = {'r_ulr' : (to_where.real_ulr +"&coor="+to_where.coor)};
+          
+          
+          /**此处还需加入转手相关代码 */
+           http.get('http://api.map.baidu.com/location/ip?ak=wROHF9YIK6wRPbf4fY2tmkur&coor=bd09ll', function(res) {
+            //  var obj='\''+res+'\'';
+            //   var obj = JSON.parse(res); 
+            res.setEncoding('utf8'); 
+            res.on('data', function (chunk) {  
+              var obj = JSON.parse(chunk); 
+                    console.log("back address:"+ obj.address); 
+                     test =obj.content ;
+                });
+            }).on('error', function(e) {
+              console.log("Got error11: " );
+            }); 
+//           console.log("test : " + test.point);
+            data  = test.point;
+//           console.log("x:"+ result_obj);
+//            if(!test) var resullt_t1 =JSON.parse(test); 
+//                    console.log("back content:"+ resullt_t1.content); 
+////            var resullt = (test.content).point;
+//            var data ={'x_point' : resullt_t2.x ,"y_point" : resullt_t2.y};
+           
+           
+      }
+//      else if( ture_ulr == "http://api2.juheapi.com/park/query?ak=45fd7fdd2d047d923e17ab08efa68554")
+      else
+      {//聚合数据 add find parking
+          //  var ulr= "http://api2.juheapi.com/park/query";
+          //  var ak = "45fd7fdd2d047d923e17ab08efa68554";
+          //  console.log("x:" + x_p.toString());
+          //  console.log("y:" +y_p.toString());
+          //  var oDis = document.getElementById('distance');
+          //  console.log("distance:" + oDis.value);
+          //  ulr = ulr + "?" + "key=" + ak + "&" + "lon=" + x_p + "&" + "lat=" + y_p + "&"+"distance="+oDis.value+"&limit=10";
+//          var t_coor = to_where.coor;
+
+           console.log("2");
+           //http://api2.juheapi.com/park/query?key=45fd7fdd2d047d923e17ab08efa68554&lon=116.3635870000&lat=39.9663730000&distance=1000&limit=10
+           http.get('http://api2.juheapi.com/park/query?key=45fd7fdd2d047d923e17ab08efa68554&lon=116.3654190000&lat=39.9700500000&distance=1000&limit=5', function(res) {
+            //  var obj='\''+res+'\'';
+            //   var obj = JSON.parse(res); 
+            res.setEncoding('utf8'); 
+            res.on('data', function (chunk) {  
+              var obj = JSON.parse(chunk); 
+                   // console.log("back address:"+ obj.address); 
+//                 console.log( "ddd: " + (((obj.result))[0]).CCDZ  );
+//           data = {'r2_ulr' : (to_where.real_ulr)};
+                    console.log("back sussed.");
+                    test= obj;
+//                    console.log( "ddd: " + (((data.result))[0]).CCDZ  );
+
+//             data ={'x_point' : 'aaa'};
+
+                    // test =obj.content ;
+                });
+            }).on('error', function(e) {
+              console.log("Got error11: " );
+            });
+             data  = test;
+      }
+      
       if (params.query && params.query.callback) {
-        //console.log(params.query.callback);
+//        //console.log(params.query.callback);
+//        console.log( "ddd: " + (((data.result))[0]).CCDZ  );
         var str =  params.query.callback + '(' + JSON.stringify(data) + ')';//jsonp
         res.end(str);
       } else {
@@ -63,6 +130,15 @@ http.createServer(function(req, res){
       }     
   }
   
+  
+//  var ulr= "http://api2.juheapi.com/park/query";
+//  var ak = "45fd7fdd2d047d923e17ab08efa68554";
+//  console.log("x:" + x_p.toString());
+//  console.log("y:" +y_p.toString());
+//  var oDis = document.getElementById('distance');
+//  console.log("distance:" + oDis.value);
+//  ulr = ulr + "?" + "key=" + ak + "&" + "lon=" + x_p + "&" + "lat=" + y_p + "&"+"distance="+oDis.value+"&limit=10";
+
   
 }).listen(port, function(){
   console.log('server is listening on port ' + port);  
